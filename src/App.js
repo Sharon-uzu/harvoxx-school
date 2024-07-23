3e968fc1500ef7d75cc0e0e637107d65f96ab5e1
Sharon-uzu <victorysharon0@gmail.com>
2024-07-23 08:31:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React from 'react';
-import Navbar from './ComponentsFolder/Navbar';
+import './App.css';
+import About from './Screens/About';
+import Apply from './Screens/Apply';
+import Home from './Screens/Home';
 import { Route, Routes } from 'react-router-dom';
-import Homepage from './Screen/Homepage';
-import Faq from './Screen/Faq';
-import Dsp100 from './Screen/Dsp100';
-import Dsp300 from './Screen/Dsp300';
-import ApplicationForm from './Screen/ApplicationForm';
-import Congrat from './Screen/Congrat';
-// import Contact from './ComponentsFolder/Contact';
-
+import Sponsors from './Screens/Sponsors';
+import Python from './Screens/Python';
+import Steps from './Screens/Steps';
+import Contact from './Screens/Contact';
 
 function App() {
   return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply' element={<Apply />} />
+        <Route path='/sponsors' element={<Sponsors />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/python' element={<Python />} />
+        <Route path='/steps' element={<Steps />} />
+        <Route path='/contact' element={<Contact />} />
 
-    <div>
-      {/* <Navbar/> */}
-        <Routes>
-            
-          <Route path='/' element={<Homepage />} />
-          <Route path='/faq' element={<Faq />} />
-          <Route path='/dsp100' element={<Dsp100 />} />
-          <Route path='/dsp300' element={<Dsp300 />} />
-          <Route path='/form' element={<ApplicationForm />} />
-          <Route path='/congrat' element={<Congrat/>} />
-          {/* <Route path='/contact' element={<Contact/>} /> */}
-
-          
-        </Routes>
-
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'; 
 
 
@@ -19,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
